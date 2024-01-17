ed702a86446bde941beea74c4a452906e666bebb
melli0505 <dmelli0505@gmail.com>
2024-01-17 15:04:36 +0900

    Notification Window

---
+++ NotificationWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace DesktopAlarm
+{
+    // MQTT Message Alarm Window
+    public partial class NotificationWindow : Window
+    {
+        public NotificationWindow(string message)
+        {
+            InitializeComponent();
+            
+            // Set Message
+            MessageTextBlock.Text = message;
+
+            // Set Close Button
+            ConfirmBtn.Click += CloseButton_Click;
+        }
+
+        // Button onclick event handler
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+} 
