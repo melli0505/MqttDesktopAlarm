e164dfe3f87687e4edf0c2dfc7a1e9cbf19c7dbc
melli0505 <dmelli0505@gmail.com>
2024-01-17 15:04:05 +0900

    MQTT Client Control - Main Thread

---
+++ App.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System.Windows;
+using System.Windows.Threading;
+using MQTTnet;
+using MQTTnet.Client;
+
+namespace DesktopAlarm
+{
+    public partial class App : Application
+    {
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await ClientTest();
+        }
+
+        // MQTT Client setting
+        public async Task ClientTest()
+        {
+            // Make Connection Setting
+            var factory = new MqttFactory();
+            var mqttClient = factory.CreateMqttClient();
+            var options = new MqttClientOptionsBuilder().WithTcpServer("BROKER_ADDRESS", 1883).Build();
+
+            await mqttClient.ConnectAsync(options, CancellationToken.None);
+
+            // Subscribe Topic
+            var mqttSubscribeOptions = factory.CreateSubscribeOptionsBuilder().WithTopicFilter(
+                f =>
+                {
+                    f.WithTopic("TOPIC");
+                }
+            ).Build();
+
+            await mqttClient.SubscribeAsync(mqttSubscribeOptions, CancellationToken.None);
+
+            // On message handler
+            mqttClient.ApplicationMessageReceivedAsync += e =>
+            {
+                var message = e.ApplicationMessage.ConvertPayloadToString();
+                // Create Alarm window
+                OnMessageReceived(message);
+                Console.WriteLine("Received application message.");
+                Console.WriteLine(message);
+                return Task.CompletedTask;
+            };
+
+            Console.WriteLine("MQTT Client subscribed to topic.");
+            Console.WriteLine("Press enter to exit.");
+            Console.ReadLine();
+
+        }
+        public void OnMessageReceived(String message)
+        {
+            // Create UI Thread
+            Dispatcher.Invoke(() =>
+            {
+                var notificationWindow = new NotificationWindow(message);
+                notificationWindow.Show();
+            });
+        }
+
+    }
+
+
+}
+
+
